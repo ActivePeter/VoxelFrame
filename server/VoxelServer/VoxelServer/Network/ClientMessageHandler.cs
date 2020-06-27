754340b80203e8fa16850ddb15b7412a1a81d018
ActivePeter <1020401660@qq.com>
2020-06-27 23:02:37 +0800

    Update ClientMessageHandler.cs

--- server/VoxelServer/VoxelServer/Network/ClientMessageHandler.cs
+++ server/VoxelServer/VoxelServer/Network/ClientMessageHandler.cs
@@ -8,9 +8,17 @@ namespace VoxelServer.Network
     {
         public const int BUFFER_SIZE = 1024;
         public byte[] buffer = new byte[BUFFER_SIZE];
+        public byte[] previousData;//用来存储之前接收但没接受全的数据
         public void handleReceive(int datalen)
         {
+            if (previousData==null)
+            {
 
+            }
+            else
+            {
+
+            }
         }
     }
 }
