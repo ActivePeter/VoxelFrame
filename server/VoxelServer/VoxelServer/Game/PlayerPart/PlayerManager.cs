3cca0d31af19abfa982376fbe611fa312d274777
ActivePeter <1020401660@qq.com>
2020-06-30 20:39:50 +0800

    文件操作

--- server/VoxelServer/VoxelServer/Game/PlayerPart/PlayerManager.cs
+++ server/VoxelServer/VoxelServer/Game/PlayerPart/PlayerManager.cs
@@ -1,21 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using VoxelServer.Utils;
 
 namespace VoxelServer.Game.PlayerPart
 {
     class PlayerManager
     {
+        public static PlayerManager instance;
         Dictionary<int, Player> playerDic=new Dictionary<int, Player>();
 
         public PlayerManager()
         {
+            instance = this;
             loadPlayers();
         }
         private void loadPlayers()
         {
-            Console.WriteLine(System.IO.Directory.GetCurrentDirectory());
-
+            //Console.WriteLine(System.IO.Directory.GetCurrentDirectory());
+            FileUtils.createIfNotExist("data");
+            FileUtils.createIfNotExist("data/playerData");
+            
         }
         
     }
---
+++ server/VoxelServer/VoxelServer/Utils/FileUtils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace VoxelServer.Utils
+{
+    class FileUtils
+    {
+        public static void createIfNotExist(String path)
+        {
+            String absPath = getAbsolutePath(path);
+            if (System.IO.Directory.Exists(absPath) == false)//如果不存在就创建file文件夹
+            {
+                Console.WriteLine("file:\"" + path + "\" not exists\r\n"+"createdAt:"+ absPath);
+
+                System.IO.Directory.CreateDirectory(absPath);
+            }
+        }
+        public static String getAbsolutePath(String path)
+        {
+            return getPrjRootPath() + Path.DirectorySeparatorChar + path;
+        }
+        public static String getPrjRootPath()
+        {
+            return System.IO.Directory.GetCurrentDirectory();
+        }
+    }
+}
