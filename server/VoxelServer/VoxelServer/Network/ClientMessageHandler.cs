01e2c8e257611479b5da4a85fcfa49d6447d03c6
ActivePeter <1020401660@qq.com>
2020-07-07 03:12:40 +0800

    Update ClientMessageHandler.cs
    将数组拼接换成了list，提高数据拼接的性能

--- server/VoxelServer/VoxelServer/Network/ClientMessageHandler.cs
+++ server/VoxelServer/VoxelServer/Network/ClientMessageHandler.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using ProtoBuf;
+using System;
 using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
 using System.Text;
 
 namespace VoxelServer.Network
@@ -8,6 +11,7 @@ namespace VoxelServer.Network
     {
         const int BUFFER_SIZE = 1024;
         public byte[] buffer = new byte[BUFFER_SIZE];
+        
         public int beginIndex = 0;
         int bodyLen = -1;//记录读出的bodylen。-1即为未读.每次处理完数据体后都要置-1                    
                          //bodyLen为数据体总长度
@@ -18,15 +22,15 @@ namespace VoxelServer.Network
                 return BUFFER_SIZE-beginIndex;
             }
         }
-
-        public byte[] incompleteBody;//用来存储之前接收但没接受全的数据体
+        List<byte> incompleteBody=new List<byte>();
+        //public byte[] incompleteBody;//用来存储之前接收但没接受全的数据体
 
         public void handleReceive(int dataLen)
         {
             int handledLen=0;//记录已经处理的数据长度。等价于下一步数据从哪开始.处理完数据就要+1
             while (handledLen!=dataLen)
             {
-                if (incompleteBody == null)//判断是否存在未接收完的数据体，不存在即先读取数据头（数据体长度int32）
+                if (incompleteBody.Count==0)//判断是否存在未接收完的数据体，不存在即先读取数据头（数据体长度int32）
                 {
                     if (dataLen-handledLen < 4)//判断长度是否达到int32，不到直接return，继续下一次接收。
                     {
@@ -41,16 +45,16 @@ namespace VoxelServer.Network
                     {
                         bodyLen = BitConverter.ToInt32(buffer, handledLen);
                         handledLen += 4;
-                        if (bodyLen > dataLen - handledLen)//数据体长度大于未处理数据长度，把数据体存入previousData
+                        if (bodyLen > dataLen - handledLen)//数据体长度大于未处理数据长度，把数据体存入incompleteBody
                         {
-                            Array.Copy(buffer, handledLen, incompleteBody, 0, dataLen - handledLen);//存入incompleteBody
+                            incompleteBody.AddRange(buffer.Skip(handledLen).Take(dataLen - handledLen));//存入incompleteBody
 
                             return;
                         }
                         else
                         {
                             //处理数据体
-
+                            //Serializer.Deserialize
                             //处理完后
                             handledLen += bodyLen;
                             resetBodyLen();
@@ -60,23 +64,23 @@ namespace VoxelServer.Network
                 else
                 {
 
-                    int leftBodyLen = bodyLen - incompleteBody.Length;
+                    int leftBodyLen = bodyLen - incompleteBody.Count;
                     if (leftBodyLen > dataLen)
                     {
-                        Array.Copy(buffer, 0, incompleteBody, incompleteBody.Length - 1, dataLen);//存入incompleteBody
+                        incompleteBody.AddRange(buffer.Take(dataLen));//存入incompleteBody
 
                         return;
                     }
                     else
                     {
-                        Array.Copy(buffer, 0, incompleteBody, incompleteBody.Length - 1, leftBodyLen);//组合数据体
+                        incompleteBody.AddRange(buffer.Take(leftBodyLen));//组合数据体
 
                         //处理数据体
 
                         //处理完后
                         handledLen += leftBodyLen;
                         resetBodyLen();
-                        incompleteBody = null;
+                        incompleteBody.Clear();
                     }
                 }
             }
