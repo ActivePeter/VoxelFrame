da0f29697e8520f1c9664083197c71ed67881b0e
ActivePeter <1020401660@qq.com>
2020-12-20 04:05:13 +0800

    no message

---
+++ client/rust_bevy/src/ecs_demo_main.rs
@@ -0,0 +1,22 @@
+// use bevy::prelude::*;
+// struct Person;
+// struct Name(String);
+// fn add_people(mut commands: Commands) {
+//     commands
+//         .spawn((Person, Name("Elaina Proctor".to_string())))
+//         .spawn((Person, Name("Renzo Hume".to_string())))
+//         .spawn((Person, Name("Zayna Nieves".to_string())));
+// }
+// fn hello_world() {
+//     println!("hello world!");
+// }
+// fn greet_people(_person: &Person, name: &Name) {
+//     println!("hello {}!", name.0);
+// }
+// fn main() {
+//     App::build()
+//         .add_startup_system(add_people.system())
+//         .add_system(hello_world.system())
+//         .add_system(greet_people.system())
+//         .run();
+// }
