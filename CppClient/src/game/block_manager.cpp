894dc08a1d87c29ffd858b1fa772d5eba83a5f83
ActivePeter <1020401660@qq.com>
2021-07-11 23:06:33 +0800

    x

--- CppClient/src/game/block_manager.cpp
+++ CppClient/src/game/block_manager.cpp
@@ -1,4 +1,6 @@
 #include "block.h"
 CommonBlockInfo *BlockManager::getBlcokInfoByBlockPos(int bx, int by, int bz)
 {
+    //1.获取区块
+    App::getInstance().gamePtr->chunkManager->getChunkOfKeyIfLoaded();
 }
